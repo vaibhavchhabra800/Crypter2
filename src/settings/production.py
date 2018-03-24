5e23a2bbe0fad50856637335b44adaaee07ab7b9
Vaibhav <vaibhavchhabra800@gmail.com>
2018-03-24 19:06:55 +0530

    changed static files location

--- src/settings/production.py
+++ src/settings/production.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env","static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_in_env","static_root")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_in_pro","our_static"),
